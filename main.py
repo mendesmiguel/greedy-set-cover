09d79d2d5384217a0b8fce78bc8c555da9165da0
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-11-07 12:09:13 -0200

    local search with first improving

--- main.py
+++ main.py
@@ -38,16 +38,16 @@ if __name__ == "__main__":
 
 	# c = np.array([5, 10, 3]).astype(float)
 
-	# esse exemplo nao da otimo: 2
-	A = np.array([
-					[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-					[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-					[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
-					]).T
+	# # esse exemplo nao da otimo: 2
+	# A = np.array([
+	# 				[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	# 				[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	# 				[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	# 				[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	# 				[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
+	# 				]).T
 
-	c = np.array([1, 1, 1, 1, 1]).astype(float)
+	# c = np.array([1, 1, 1, 1, 1]).astype(float)
 
 	# # esse exemplo nao da otimo - otimo: 22
 	# A = np.array([
@@ -58,16 +58,16 @@ if __name__ == "__main__":
 
 	# c = np.array([6, 15, 7]).astype(float)
 
-	# fh = FileHandler('datasets/scpa1.txt')
-	# A, c = fh.process()
+	fh = FileHandler('datasets/scpa1.txt')
+	A, c = fh.process()
 
-	print "A = ", A
-	print "c = ", c
+	print "A shape = ", A.shape
+	# print "c = ", c
 	print "len(c) = ", len(c)
 	print "\n\n\n"
 
 	s = Solver(A, c)
-	s.solve(0.5, 3)
+	s.solve(0.5, 5)
 	s.print_solution()
 	print "solution as sets: {0}".format(s.get_solution_as_sets())
 	# print "solution as matrix A: {0}".format(s.get_solution_as_matrix())
--- set_cover/solver.py
+++ set_cover/solver.py
@@ -19,15 +19,16 @@ class Solver(object):
 			self.A_copy = self.A.copy()
 			self.c_copy = self.c.copy()
 			print "iteration {0}:".format(i)
-			print "best solution found so far {0} has cost: {1}".format(best_sol, self._get_cost(best_sol))
+			# print "best solution found so far {0} has cost: {1}".format(best_sol, self._get_cost(best_sol))
 			solution = self._greedy_randomized_construction(alpha)
-			print "greedy construction generated solution {0} with cost: {1}".format(solution, self._get_cost(solution))
+			print "greedy construction generated solution with cost: {0}".format(self._get_cost(solution))
 			solution = self._local_search(solution)
-			print "local search generated solution {0} with cost: {1}".format(solution, self._get_cost(solution))
+			print "local search generated solution with cost: {0}".format(self._get_cost(solution))
+			# print "local search generated solution with cost: {0}".format(self._get_cost(solution))
 
 			if self._get_cost(solution) < self._get_cost(best_sol): 
 				best_sol = solution
-				print "local search produced a better solution {0} with cost: {1}".format(best_sol, self._get_cost(solution))
+			print "best solution so far has cost:: {0}".format(self._get_cost(best_sol))
 		self.S = np.where(best_sol == True)[0].tolist()
 		self.total_cost = self._get_cost(best_sol)
 
@@ -40,16 +41,17 @@ class Solver(object):
 			sol_copy = sol.copy()
 			sol_copy[i] = not sol_copy[i]
 			A = self.A.copy()
-			print "	best solution found so far {0} has cost: {1}".format(best_sol, self._get_cost(best_sol))
-			print "	solution {0}: {1}".format(i, sol_copy)
-			print "	A: {0}".format(A)
+			# print "	best solution found so far {0} has cost: {1}".format(best_sol, self._get_cost(best_sol))
+			# print "	solution {0}: {1}".format(i, sol_copy)
+			# print "	A: {0}".format(A)
 			self._ls_helper(sol_copy, A)
 			if self._is_feasible(sol_copy, A):
 				cost = self._get_cost(sol_copy)
-				print "	solution {0} is feasible and has cost: {1}".format(sol_copy, cost)
+				# print "	solution {0} is feasible and has cost: {1}".format(sol_copy, cost)
 				if cost < best_sol_cost:
 					best_sol_cost = cost
 					best_sol = sol_copy
+					break
 		return best_sol
 
 	def _ls_helper(self, solution, A):
@@ -58,27 +60,30 @@ class Solver(object):
 
 	def _greedy_randomized_construction(self, alpha):
 		solution = np.zeros(self.n, dtype=bool)
-		print "### greedy randomized construction ###"
-		print "	solution: {0}".format(solution)
+		# print "### greedy randomized construction ###"
+		# print "	solution: {0}".format(solution)
 		while not self._is_feasible(solution, self.A_copy):
 		# for i in range(3):
 			# self._is_feasible(solution)
 			rcl = self._get_rcl(alpha)
-			print "	rlc: {0}".format(rcl)
+			# print "	rlc: {0}".format(rcl)
 			v = self._get_candidate(rcl)
-			print "	chosen candidate: {0}".format(v)
+			# print "	chosen candidate: {0}".format(v)
 			solution[v] = True
 			self.c_copy[v] = 0
-			print "	solution: {0}".format(solution)
+			# print "	solution: {0}".format(solution)
 			self._remove_intersection(v)
 		return solution
 
 	def _get_rcl(self, alpha):
-		# card = np.sum(self.A_copy, axis=0).astype(float)
-		cost = self.c_copy
-		print "	cost: {0}".format(cost)
+		card = np.sum(self.A_copy, axis=0).astype(float)
+		cost = self.c.copy()
+		# print "	cost: {0}".format(cost)
+		# print "	card: {0}".format(card)
+		factor = card / cost
+		# print "factor {0}".format(factor)
 		n = self.A_copy.shape[1]
-		return np.argsort(cost)[::-1][:alpha * n + 1]
+		return np.argsort(factor)[::-1][:alpha * n + 1]
 
 	def _get_candidate(self, rlc):
 		return random.choice(rlc)
@@ -89,20 +94,20 @@ class Solver(object):
 
 	def _is_feasible(self, solution, A):
 		idx = np.where(solution == False)[0]
-		print "solution inside is_feasible: {0}".format(solution)
+		# print "solution inside is_feasible: {0}".format(solution)
 		res = np.sum(A[:, idx], axis=1)
-		print "res inside is_feasible: {0}".format(res)
+		# print "res inside is_feasible: {0}".format(res)
 		return np.sum(res) == 0
 
 	def _remove_intersection(self, sj):
-		print "## remove intersection ##"
-		print "	before: {0}".format(self.A_copy)
+		# print "## remove intersection ##"
+		# print "	before: {0}".format(self.A_copy)
 		p = self.A_copy[:, sj] > 0
 		# para cada coluna (set) remova a intersecao
 		# print "A = ", self.A_copy
 		for j in range(self.n):
 			self.A_copy[:, j][p] = 0
-		print "	after: {0}".format(self.A_copy)
+		# print "	after: {0}".format(self.A_copy)
 
 	def _get_collumn(self, col_idx):
 		return np.nonzero(self.A_copy[:, col_idx] > 0)[0]
