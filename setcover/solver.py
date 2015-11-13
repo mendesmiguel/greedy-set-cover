770c2a559d3582501ae9b4d3c76677f130536b48
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-11-13 21:26:20 -0200

    grasp with tabu search

--- setcover/solver.py
+++ setcover/solver.py
@@ -29,7 +29,8 @@ class Solver(object):
 			logging.info("iteration {0}:".format(i))
 			solution = self._greedy_randomized_construction(alpha)
 			logging.info("greedy construction generated solution with cost: {0}".format(self._get_cost(solution)))
-			solution = self._local_search(solution)
+			solution = self._tabu_search(solution)
+			logging.info("tabu search generated solution with cost: {0}".format(self._get_cost(solution)))
 
 			if self._get_cost(solution) < self._get_cost(best_sol): 
 				best_sol = solution
@@ -37,22 +38,75 @@ class Solver(object):
 		self.S = np.where(best_sol == True)[0].tolist()
 		self.total_cost = self._get_cost(best_sol)
 
-	def _local_search(self, sol):
-		best_sol_cost = self._get_cost(sol)
-		best_sol = sol.copy()
+	# def _tabu_search(self, sol):
+	# 	s = sol.copy()
+	# 	s_best = sol.copy()
+	# 	tabu_list = []
 
-		for i in range(len(sol)):
-			sol_copy = sol.copy()
-			sol_copy[i] = not sol_copy[i]
-			A = self.A.copy()
+	# 	for j in range(5):
+	# 		best_candidate = np.ones(self.n, dtype=bool)
+	# 		logging.info("tabu search iteration {0}:".format(j))
+	# 		logging.info("s_best cost {0}:".format(self._get_cost(s_best)))
+	# 		logging.info("s cost {0}:".format(self._get_cost(s)))
+	# 		for i in range(len(s)):
+	# 			s_candidate = s.copy()
+	# 			s_candidate[i] = not s_candidate[i]
+	# 			A = self.A.copy()
 
-			if self._is_feasible(sol_copy, A):
-				cost = self._get_cost(sol_copy)
-				if cost < best_sol_cost:
-					logging.info("local search produced solution with cost: {0}".format(cost))
-					best_sol_cost = cost
-					best_sol = sol_copy
-		return best_sol
+	# 			if not self._is_feasible(s_candidate, A):
+	# 				continue
+
+	# 			if not any((s_candidate == e).all() for e in tabu_list) and \
+	# 				(self._get_cost(s_candidate) < self._get_cost(best_candidate)):
+	# 				best_candidate = s_candidate
+
+	# 			s = best_candidate
+
+	# 			if self._get_cost(best_candidate) < self._get_cost(s_best):
+	# 				s_best = best_candidate
+	# 				logging.info("tabu search found a better solution with cost: {0}".format(self._get_cost(s_best)))
+	# 			tabu_list.append(best_candidate)
+
+	# 	return s_best
+
+	def _tabu_search(self, sol):
+		# logging.info("tabu search called")
+		s_best = sol.copy()
+		s = sol.copy()
+		it = 0
+		best_it = 0
+		bt_max = 5
+		tabu_list = []
+		
+		while (it - best_it) <= bt_max:
+			it += 1
+			logging.info("tabu search iteration {0}:".format(it))
+			s_candidate = self._get_best_neighbor(s)
+			logging.info("s_candidate cost: {0}".format(self._get_cost(s_candidate)))
+			if any((s_candidate == e).all() for e in tabu_list) or \
+				self._get_cost(s_candidate) >= self._get_cost(s):
+				continue
+
+			logging.info("adding solution with cost: {0} to tabu list".format(self._get_cost(s_candidate)))
+			tabu_list.append(s_candidate)
+			s = s_candidate
+
+			if self._get_cost(s) < self._get_cost(s_best):
+				s_best = s.copy()
+				best_it = it
+				logging.info("tabu search found a better solution with cost: {0}".format(self._get_cost(s_best)))
+		return s_best
+
+	def _get_best_neighbor(self, s):
+		A = self.A.copy()
+		best_s = s.copy()
+		for i in range(len(s)):
+			s_candidate = s.copy()
+			s_candidate[i] = not s_candidate[i]
+
+			if self._is_feasible(s_candidate, A) and self._get_cost(s_candidate) < self._get_cost(best_s):
+				best_s = s_candidate.copy()
+		return best_s
 
 	def _ls_helper(self, solution, A):
 		idx = np.where(solution == True)[0]
