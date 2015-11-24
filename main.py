52fec3dc14738866cd54a349b3594e5c24dcb38f
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-11-24 20:39:59 -0200

    convert tabs to spaces

--- main.py
+++ main.py
@@ -7,70 +7,70 @@ import sys
 if __name__ == "__main__":
 
 
-	# A = np.array([
-	# 				[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	# 				[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-	# 				[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-	# 				[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-	# 				[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
-	# 				]).T
+    # A = np.array([
+    #               [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #               [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    #               [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    #               [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
+    #               ]).T
 
-	# c = np.array([1, 1, 1, 1, 1]).astype(float)
+    # c = np.array([1, 1, 1, 1, 1]).astype(float)
 
 
 
-	# A = np.array([
-	# 				[1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-	# 				[1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-	# 				[0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
-	# 				[0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
-	# 				[0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
-	# 				[0, 0, 1, 1, 0, 0, 0, 0, 0, 1]
-	# 				])
+    # A = np.array([
+    #               [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    #               [1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    #               [0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
+    #               [0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+    #               [0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
+    #               [0, 0, 1, 1, 0, 0, 0, 0, 0, 1]
+    #               ])
 
-	# c = np.array([3, 3, 3, 14, 1, 1, 1, 1, 1, 1]).astype(float)
+    # c = np.array([3, 3, 3, 14, 1, 1, 1, 1, 1, 1]).astype(float)
 
-	# A = np.array([
-	# 				[1, 0, 1],
-	# 				[0, 1, 1],
-	# 				[1, 0, 1],
-	# 				[1, 0, 1],
-	# 				[0, 1, 0]
-	# 				])
+    # A = np.array([
+    #               [1, 0, 1],
+    #               [0, 1, 1],
+    #               [1, 0, 1],
+    #               [1, 0, 1],
+    #               [0, 1, 0]
+    #               ])
 
-	# c = np.array([5, 10, 3]).astype(float)
+    # c = np.array([5, 10, 3]).astype(float)
 
-	# # esse exemplo nao da otimo: 2
-	# A = np.array([
-	# 				[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	# 				[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-	# 				[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-	# 				[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-	# 				[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
-	# 				]).T
+    # # esse exemplo nao da otimo: 2
+    # A = np.array([
+    #               [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #               [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    #               [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    #               [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
+    #               ]).T
 
-	# c = np.array([1, 1, 1, 1, 1]).astype(float)
+    # c = np.array([1, 1, 1, 1, 1]).astype(float)
 
-	# # esse exemplo nao da otimo - otimo: 22
-	# A = np.array([
-	# 				[0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-	# 				[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-	# 				[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
-	# 				]).T
+    # # esse exemplo nao da otimo - otimo: 22
+    # A = np.array([
+    #               [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    #               [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    #               [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    #               ]).T
 
-	# c = np.array([6, 15, 7]).astype(float)
+    # c = np.array([6, 15, 7]).astype(float)
 
-	total = len(sys.argv)
-	if total != 5:
-		print "Usage: ./main.py [dataset] [logfilename] [alpha] [num of iterations]" 
-		# print "Given: ", sys.argv
-	else:
-		# print "Given: ", sys.argv
-		dataset, logfile, alpha, N = sys.argv[1:]
-		fh = FileHandler(dataset)
-		A, c = fh.process()
-		s = Solver(A, c, logfile, TabuSearch(float(alpha), int(N)))
-		s.solve()
-		s.print_total_cost()
-		# print "solution as sets: {0}".format(s.get_solution_as_sets())
-	# print "solution as matrix A: {0}".format(s.get_solution_as_matrix())
+    total = len(sys.argv)
+    if total != 5:
+        print "Usage: ./main.py [dataset] [logfilename] [alpha] [num of iterations]" 
+        # print "Given: ", sys.argv
+    else:
+        # print "Given: ", sys.argv
+        dataset, logfile, alpha, N = sys.argv[1:]
+        fh = FileHandler(dataset)
+        A, c = fh.process()
+        s = Solver(A, c, logfile, TabuSearch(float(alpha), int(N)))
+        s.solve()
+        s.print_total_cost()
+        # print "solution as sets: {0}".format(s.get_solution_as_sets())
+    # print "solution as matrix A: {0}".format(s.get_solution_as_matrix())
--- setcover/solver.py
+++ setcover/solver.py
@@ -3,225 +3,225 @@ import random
 import logging
 
 class Solver(object):
-	def __init__(self, A, c, problem_name, search_strategy):
-		self.A = A
-		self.c = c
-		self.m, self.n = self.A.shape
-		self.problem_name = problem_name
-		self.search_strategy = search_strategy
-		self.S = None
-		self.search_strategy.set_up(A, c, problem_name)
+    def __init__(self, A, c, problem_name, search_strategy):
+        self.A = A
+        self.c = c
+        self.m, self.n = self.A.shape
+        self.problem_name = problem_name
+        self.search_strategy = search_strategy
+        self.S = None
+        self.search_strategy.set_up(A, c, problem_name)
 
-	def solve(self):
-		self.search_strategy.solve()
+    def solve(self):
+        self.search_strategy.solve()
 
-	def print_solution(self):
-		self.search_strategy.print_solution()
-	def print_total_cost(self):
-		print self.search_strategy.get_total_cost()
+    def print_solution(self):
+        self.search_strategy.print_solution()
+    def print_total_cost(self):
+        print self.search_strategy.get_total_cost()
 
 class AbstractSearchStrategy(object):
-	def _greedy_randomized_construction(self, alpha):
-		solution = np.zeros(self.n, dtype=bool)
+    def _greedy_randomized_construction(self, alpha):
+        solution = np.zeros(self.n, dtype=bool)
 
-		A = self.A.copy()
-		while not self._is_feasible(solution, A):
-			rcl = self._get_rcl(alpha)
-			v = self._get_candidate(rcl)
-			solution[v] = True
-			self.c_copy[v] = 0
-			self._remove_intersection(v)
-		return solution
+        A = self.A.copy()
+        while not self._is_feasible(solution, A):
+            rcl = self._get_rcl(alpha)
+            v = self._get_candidate(rcl)
+            solution[v] = True
+            self.c_copy[v] = 0
+            self._remove_intersection(v)
+        return solution
 
 
-	def set_up(self, A, c, problem_name):
-		self.A = A
-		self.c = c
-		self.A_copy = A.copy()
-		self.c_copy = c.copy()
-		self.m, self.n = self.A_copy.shape
-		self.total_cost = 0
-		self.S = None
-		logging.basicConfig(filename=problem_name+'.log',
-							level=logging.DEBUG,
-							format='%(asctime)s %(message)s', 
-							datefmt='%m/%d/%Y %I:%M:%S %p')
+    def set_up(self, A, c, problem_name):
+        self.A = A
+        self.c = c
+        self.A_copy = A.copy()
+        self.c_copy = c.copy()
+        self.m, self.n = self.A_copy.shape
+        self.total_cost = 0
+        self.S = None
+        logging.basicConfig(filename=problem_name+'.log',
+                            level=logging.DEBUG,
+                            format='%(asctime)s %(message)s', 
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
 
-	def _get_rcl(self, alpha):
-		card = np.sum(self.A_copy, axis=0).astype(float)
-		cost = self.c.copy()
-		factor = card / cost
-		n = self.A_copy.shape[1]
-		return np.argsort(factor)[::-1][:alpha * n + 1]
+    def _get_rcl(self, alpha):
+        card = np.sum(self.A_copy, axis=0).astype(float)
+        cost = self.c.copy()
+        factor = card / cost
+        n = self.A_copy.shape[1]
+        return np.argsort(factor)[::-1][:alpha * n + 1]
 
-	def _get_candidate(self, rlc):
-		return random.choice(rlc)
+    def _get_candidate(self, rlc):
+        return random.choice(rlc)
 
-	def _get_cost(self, sol):
-		cost = np.sum(self.c[sol])
-		return cost
+    def _get_cost(self, sol):
+        cost = np.sum(self.c[sol])
+        return cost
 
-	def _is_feasible(self, solution, A):
-		idx = np.where(solution == True)[0]
-		res = np.sum(A[:, idx], axis=1)
-		return not (0 in res)
+    def _is_feasible(self, solution, A):
+        idx = np.where(solution == True)[0]
+        res = np.sum(A[:, idx], axis=1)
+        return not (0 in res)
 
-	def _remove_intersection(self, sj):
-		p = self.A_copy[:, sj] > 0
+    def _remove_intersection(self, sj):
+        p = self.A_copy[:, sj] > 0
 
-		for j in range(self.n):
-			self.A_copy[:, j][p] = 0
+        for j in range(self.n):
+            self.A_copy[:, j][p] = 0
 
-	def _get_collumn(self, col_idx):
-		return np.nonzero(self.A_copy[:, col_idx] > 0)[0]
+    def _get_collumn(self, col_idx):
+        return np.nonzero(self.A_copy[:, col_idx] > 0)[0]
 
-	def _get_universe(self):
-		return set([i for i in range(self.m)])
+    def _get_universe(self):
+        return set([i for i in range(self.m)])
 
-	def get_solution_as_sets(self):
-		return [self._get_set_by_index(sj) for sj in self.S]
+    def get_solution_as_sets(self):
+        return [self._get_set_by_index(sj) for sj in self.S]
 
-	def get_total_cost(self):
-		return self.total_cost
+    def get_total_cost(self):
+        return self.total_cost
 
-	def get_solution_as_matrix(self):
-		A_copy = self.A.copy()
-		return A_copy[:, [sj for sj in self.S]]
+    def get_solution_as_matrix(self):
+        A_copy = self.A.copy()
+        return A_copy[:, [sj for sj in self.S]]
 
-	def _get_set_by_index(self, j):
-		pj = np.nonzero(self.A[:, j] > 0)[0].tolist()
-		return pj
+    def _get_set_by_index(self, j):
+        pj = np.nonzero(self.A[:, j] > 0)[0].tolist()
+        return pj
 
-	def print_solution(self):
-		print "# original sets: "
+    def print_solution(self):
+        print "# original sets: "
 
-		for j in range(self.n):
-			print "S%d: %s -- cost: %.3f" % (j, self._get_set_by_index(j), self.c[j])
-			logging.info("S%d: %s -- cost: %.3f" % (j, self._get_set_by_index(j), self.c[j]))
-		print "# solution: "
-		logging.info("# solution: ")
+        for j in range(self.n):
+            print "S%d: %s -- cost: %.3f" % (j, self._get_set_by_index(j), self.c[j])
+            logging.info("S%d: %s -- cost: %.3f" % (j, self._get_set_by_index(j), self.c[j]))
+        print "# solution: "
+        logging.info("# solution: ")
 
-		for sj in self.S:
-			print "S%d: %s" % (sj, self._get_set_by_index(sj))
-			logging.info("S%d: %s" % (sj, self._get_set_by_index(sj)))
-		print "Total cost: %.3f" % (self.total_cost)
-		logging.info("Total cost: %.3f" % (self.total_cost))
+        for sj in self.S:
+            print "S%d: %s" % (sj, self._get_set_by_index(sj))
+            logging.info("S%d: %s" % (sj, self._get_set_by_index(sj)))
+        print "Total cost: %.3f" % (self.total_cost)
+        logging.info("Total cost: %.3f" % (self.total_cost))
 
 class LocalSearch(AbstractSearchStrategy):
 
-	def __init__(self, alpha, N):
-		self.alpha = alpha
-		self.N = N
+    def __init__(self, alpha, N):
+        self.alpha = alpha
+        self.N = N
 
-	def solve(self):
-		alpha = self.alpha
-		N = self.N
+    def solve(self):
+        alpha = self.alpha
+        N = self.N
 
-		best_sol = np.ones(self.n, dtype=bool)
-		logging.info("A shape: {0}".format(self.A.shape))
-		logging.info("N iterations: {0}".format(N))
-		logging.info("alpha: {0}".format(alpha))
-		logging.info("RCL length: {0}".format(len(self._get_rcl(alpha))))
-		for i in range(N):
-			self.A_copy = self.A.copy()
-			self.c_copy = self.c.copy()
-			logging.info("iteration {0}:".format(i))
-			solution = self._greedy_randomized_construction(alpha)
-			logging.info("greedy construction generated solution with cost: {0}".format(self._get_cost(solution)))
-			solution = self._local_search(solution)
+        best_sol = np.ones(self.n, dtype=bool)
+        logging.info("A shape: {0}".format(self.A.shape))
+        logging.info("N iterations: {0}".format(N))
+        logging.info("alpha: {0}".format(alpha))
+        logging.info("RCL length: {0}".format(len(self._get_rcl(alpha))))
+        for i in range(N):
+            self.A_copy = self.A.copy()
+            self.c_copy = self.c.copy()
+            logging.info("iteration {0}:".format(i))
+            solution = self._greedy_randomized_construction(alpha)
+            logging.info("greedy construction generated solution with cost: {0}".format(self._get_cost(solution)))
+            solution = self._local_search(solution)
 
-			if self._get_cost(solution) < self._get_cost(best_sol): 
-				best_sol = solution
-			logging.info("best solution so far has cost:: {0}".format(self._get_cost(best_sol)))
-		self.S = np.where(best_sol == True)[0].tolist()
-		self.total_cost = self._get_cost(best_sol)
+            if self._get_cost(solution) < self._get_cost(best_sol): 
+                best_sol = solution
+            logging.info("best solution so far has cost:: {0}".format(self._get_cost(best_sol)))
+        self.S = np.where(best_sol == True)[0].tolist()
+        self.total_cost = self._get_cost(best_sol)
 
-	def _local_search(self, sol):
-		best_sol_cost = self._get_cost(sol)
-		best_sol = sol.copy()
+    def _local_search(self, sol):
+        best_sol_cost = self._get_cost(sol)
+        best_sol = sol.copy()
 
-		for i in range(len(sol)):
-			sol_copy = sol.copy()
-			sol_copy[i] = not sol_copy[i]
-			A = self.A.copy()
+        for i in range(len(sol)):
+            sol_copy = sol.copy()
+            sol_copy[i] = not sol_copy[i]
+            A = self.A.copy()
 
-			if self._is_feasible(sol_copy, A):
-				cost = self._get_cost(sol_copy)
-				if cost < best_sol_cost:
-					logging.info("local search produced solution with cost: {0}".format(cost))
-					best_sol_cost = cost
-					best_sol = sol_copy
-		return best_sol
+            if self._is_feasible(sol_copy, A):
+                cost = self._get_cost(sol_copy)
+                if cost < best_sol_cost:
+                    logging.info("local search produced solution with cost: {0}".format(cost))
+                    best_sol_cost = cost
+                    best_sol = sol_copy
+        return best_sol
 
-	def _ls_helper(self, solution, A):
-		idx = np.where(solution == True)[0]
-		A[:, idx] = 0
+    def _ls_helper(self, solution, A):
+        idx = np.where(solution == True)[0]
+        A[:, idx] = 0
 
 
 class TabuSearch(AbstractSearchStrategy):
-	def __init__(self, alpha, N):
-		self.alpha = alpha
-		self.N = N
+    def __init__(self, alpha, N):
+        self.alpha = alpha
+        self.N = N
 
-	def solve(self):
-		alpha = self.alpha
-		N = self.N
+    def solve(self):
+        alpha = self.alpha
+        N = self.N
 
-		best_sol = np.ones(self.n, dtype=bool)
-		logging.info("A shape: {0}".format(self.A.shape))
-		logging.info("N iterations: {0}".format(N))
-		logging.info("alpha: {0}".format(alpha))
-		logging.info("RCL length: {0}".format(len(self._get_rcl(alpha))))
-		for i in range(N):
-			self.A_copy = self.A.copy()
-			self.c_copy = self.c.copy()
-			logging.info("iteration {0}:".format(i))
-			solution = self._greedy_randomized_construction(alpha)
-			logging.info("greedy construction generated solution with cost: {0}".format(self._get_cost(solution)))
-			solution = self._tabu_search(solution)
-			logging.info("tabu search generated solution with cost: {0}".format(self._get_cost(solution)))
+        best_sol = np.ones(self.n, dtype=bool)
+        logging.info("A shape: {0}".format(self.A.shape))
+        logging.info("N iterations: {0}".format(N))
+        logging.info("alpha: {0}".format(alpha))
+        logging.info("RCL length: {0}".format(len(self._get_rcl(alpha))))
+        for i in range(N):
+            self.A_copy = self.A.copy()
+            self.c_copy = self.c.copy()
+            logging.info("iteration {0}:".format(i))
+            solution = self._greedy_randomized_construction(alpha)
+            logging.info("greedy construction generated solution with cost: {0}".format(self._get_cost(solution)))
+            solution = self._tabu_search(solution)
+            logging.info("tabu search generated solution with cost: {0}".format(self._get_cost(solution)))
 
-			if self._get_cost(solution) < self._get_cost(best_sol): 
-				best_sol = solution
-			logging.info("best solution so far has cost:: {0}".format(self._get_cost(best_sol)))
-		self.S = np.where(best_sol == True)[0].tolist()
-		self.total_cost = self._get_cost(best_sol)
+            if self._get_cost(solution) < self._get_cost(best_sol): 
+                best_sol = solution
+            logging.info("best solution so far has cost:: {0}".format(self._get_cost(best_sol)))
+        self.S = np.where(best_sol == True)[0].tolist()
+        self.total_cost = self._get_cost(best_sol)
 
-	def _tabu_search(self, sol):
-		# logging.info("tabu search called")
-		s_best = sol.copy()
-		s = sol.copy()
-		it = 0
-		best_it = 0
-		bt_max = 5
-		tabu_list = []
-		
-		while (it - best_it) <= bt_max:
-			it += 1
-			logging.info("tabu search iteration {0}:".format(it))
-			s_candidate = self._get_best_neighbor(s)
-			logging.info("s_candidate cost: {0}".format(self._get_cost(s_candidate)))
-			if any((s_candidate == e).all() for e in tabu_list) or \
-				self._get_cost(s_candidate) >= self._get_cost(s):
-				continue
+    def _tabu_search(self, sol):
+        # logging.info("tabu search called")
+        s_best = sol.copy()
+        s = sol.copy()
+        it = 0
+        best_it = 0
+        bt_max = 5
+        tabu_list = []
+        
+        while (it - best_it) <= bt_max:
+            it += 1
+            logging.info("tabu search iteration {0}:".format(it))
+            s_candidate = self._get_best_neighbor(s)
+            logging.info("s_candidate cost: {0}".format(self._get_cost(s_candidate)))
+            if any((s_candidate == e).all() for e in tabu_list) or \
+                self._get_cost(s_candidate) >= self._get_cost(s):
+                continue
 
-			logging.info("adding solution with cost: {0} to tabu list".format(self._get_cost(s_candidate)))
-			tabu_list.append(s_candidate)
-			s = s_candidate
+            logging.info("adding solution with cost: {0} to tabu list".format(self._get_cost(s_candidate)))
+            tabu_list.append(s_candidate)
+            s = s_candidate
 
-			if self._get_cost(s) < self._get_cost(s_best):
-				s_best = s.copy()
-				best_it = it
-				logging.info("tabu search found a better solution with cost: {0}".format(self._get_cost(s_best)))
-		return s_best
+            if self._get_cost(s) < self._get_cost(s_best):
+                s_best = s.copy()
+                best_it = it
+                logging.info("tabu search found a better solution with cost: {0}".format(self._get_cost(s_best)))
+        return s_best
 
-	def _get_best_neighbor(self, s):
-		A = self.A.copy()
-		best_s = s.copy()
-		for i in range(len(s)):
-			s_candidate = s.copy()
-			s_candidate[i] = not s_candidate[i]
+    def _get_best_neighbor(self, s):
+        A = self.A.copy()
+        best_s = s.copy()
+        for i in range(len(s)):
+            s_candidate = s.copy()
+            s_candidate[i] = not s_candidate[i]
 
-			if self._is_feasible(s_candidate, A) and self._get_cost(s_candidate) < self._get_cost(best_s):
-				best_s = s_candidate.copy()
-		return best_s
+            if self._is_feasible(s_candidate, A) and self._get_cost(s_candidate) < self._get_cost(best_s):
+                best_s = s_candidate.copy()
+        return best_s
