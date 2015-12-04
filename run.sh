df2316e872a0314416de22d793fffcbb5a9fb53f
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-12-04 16:46:17 -0200

    run edited

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-python main.py datasets/scp41.txt 4_1-1 0.003 3600
-python main.py datasets/scp42.txt 4_2-1 0.003 3600
-python main.py datasets/scp43.txt 4_3-1 0.003 3600
-python main.py datasets/scp44.txt 4_4-1 0.003 3600
-python main.py datasets/scp45.txt 4_5-1 0.003 3600
-python main.py datasets/scp46.txt 4_6-1 0.003 3600
-python main.py datasets/scp47.txt 4_7-1 0.003 3600
-python main.py datasets/scp48.txt 4_8-1 0.003 3600
-python main.py datasets/scp410.txt 4_10-1 0.003 3600
+nohup python main.py datasets/scp41.txt 4_1-1 0.003 3600
+nohup python main.py datasets/scp42.txt 4_2-1 0.003 3600
+nohup python main.py datasets/scp43.txt 4_3-1 0.003 3600
+nohup python main.py datasets/scp44.txt 4_4-1 0.003 3600
+nohup python main.py datasets/scp45.txt 4_5-1 0.003 3600
+nohup python main.py datasets/scp46.txt 4_6-1 0.003 3600
+nohup python main.py datasets/scp47.txt 4_7-1 0.003 3600
+nohup python main.py datasets/scp48.txt 4_8-1 0.003 3600
+nohup python main.py datasets/scp410.txt 4_10-1 0.003 3600
