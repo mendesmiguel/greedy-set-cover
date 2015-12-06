8f6589a8b2f634a064fad9bc5894ed562cea4d63
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-12-06 19:26:46 -0200

    fix tests

--- run_tabu.sh
+++ run_tabu.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-python main.py datasets/scp41.txt 4_1-1-local 0.003 3600 TabuSearch
-python main.py datasets/scp41.txt 4_1-2-local 0.005 3600 TabuSearch
-python main.py datasets/scp41.txt 4_1-3-local 0.008 3600 TabuSearch
-python main.py datasets/scp41.txt 4_1-4-local 0.010 3600 TabuSearch
+python main.py datasets/scp42.txt 4_2-1-tabu 0.003 3600 TabuSearch
 
-python main.py datasets/scp41.txt 4_2-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp42.txt 4_2-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp43.txt 4_3-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp44.txt 4_4-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp45.txt 4_5-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp46.txt 4_6-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp47.txt 4_7-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp48.txt 4_8-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp49.txt 4_9-4-local 0.010 3600 TabuSearch
-python main.py datasets/scp410.txt 4_10-4-local 0.010 3600 TabuSearch
+
+python main.py datasets/scp41.txt 4_1-2-tabu 0.005 3600 TabuSearch
+python main.py datasets/scp41.txt 4_1-3-tabu 0.008 3600 TabuSearch
+python main.py datasets/scp41.txt 4_1-4-tabu 0.010 3600 TabuSearch
+
+python main.py datasets/scp41.txt 4_2-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp42.txt 4_2-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp43.txt 4_3-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp44.txt 4_4-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp45.txt 4_5-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp46.txt 4_6-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp47.txt 4_7-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp48.txt 4_8-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp49.txt 4_9-4-tabu 0.010 3600 TabuSearch
+python main.py datasets/scp410.txt 4_10-4-tabu 0.010 3600 TabuSearch
--- run_vnd.sh
+++ run_vnd.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-python main.py datasets/scp41.txt 4_1-1-local 0.003 3600 VNDSearch
-python main.py datasets/scp41.txt 4_1-2-local 0.005 3600 VNDSearch
-python main.py datasets/scp41.txt 4_1-3-local 0.008 3600 VNDSearch
-python main.py datasets/scp41.txt 4_1-4-local 0.010 3600 VNDSearch
 
-python main.py datasets/scp41.txt 4_2-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp42.txt 4_2-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp43.txt 4_3-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp44.txt 4_4-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp45.txt 4_5-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp46.txt 4_6-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp47.txt 4_7-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp48.txt 4_8-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp49.txt 4_9-4-local 0.010 3600 VNDSearch
-python main.py datasets/scp410.txt 4_10-4-local 0.010 3600 VNDSearch
+python main.py datasets/scp42.txt 4_2-1-vnd 0.003 3600 VNDSearch
+
+python main.py datasets/scp41.txt 4_1-2-vnd 0.005 3600 VNDSearch
+python main.py datasets/scp41.txt 4_1-3-vnd 0.008 3600 VNDSearch
+python main.py datasets/scp41.txt 4_1-4-vnd 0.010 3600 VNDSearch
+
+python main.py datasets/scp41.txt 4_2-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp42.txt 4_2-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp43.txt 4_3-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp44.txt 4_4-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp45.txt 4_5-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp46.txt 4_6-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp47.txt 4_7-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp48.txt 4_8-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp49.txt 4_9-4-vnd 0.010 3600 VNDSearch
+python main.py datasets/scp410.txt 4_10-4-vnd 0.010 3600 VNDSearch
