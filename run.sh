51aca9ff79102aeca07f9f3cd0d6354fdf0e1700
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-12-06 19:23:12 -0200

    new tests

--- run.sh
+++ run.sh
@@ -1,32 +1,16 @@
 #!/bin/bash
-python main.py datasets/scp42.txt 4_2-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp43.txt 4_3-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp44.txt 4_4-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp45.txt 4_5-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp46.txt 4_6-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp47.txt 4_7-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp48.txt 4_8-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp49.txt 4_9-1-local 0.003 3600 LocalSearch
-python main.py datasets/scp410.txt 4_10-1-local 0.003 3600 LocalSearch
+python main.py datasets/scp41.txt 4_1-1-local 0.003 3600 LocalSearch
+python main.py datasets/scp41.txt 4_1-2-local 0.005 3600 LocalSearch
+python main.py datasets/scp41.txt 4_1-3-local 0.008 3600 LocalSearch
+python main.py datasets/scp41.txt 4_1-4-local 0.010 3600 LocalSearch
 
-python main.py datasets/scp41.txt 4_2-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp42.txt 4_2-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp43.txt 4_3-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp44.txt 4_4-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp45.txt 4_5-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp46.txt 4_6-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp47.txt 4_7-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp48.txt 4_8-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp49.txt 4_9-2-local 0.005 3600 LocalSearch
-python main.py datasets/scp410.txt 4_10-2-local 0.005 3600 LocalSearch
-
-python main.py datasets/scp41.txt 4_2-1-local 0.008 3600 LocalSearch
-python main.py datasets/scp42.txt 4_2-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp43.txt 4_3-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp44.txt 4_4-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp45.txt 4_5-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp46.txt 4_6-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp47.txt 4_7-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp48.txt 4_8-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp49.txt 4_9-3-local 0.008 3600 LocalSearch
-python main.py datasets/scp410.txt 4_10-3-local 0.008 3600 LocalSearch
+python main.py datasets/scp41.txt 4_2-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp42.txt 4_2-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp43.txt 4_3-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp44.txt 4_4-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp45.txt 4_5-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp46.txt 4_6-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp47.txt 4_7-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp48.txt 4_8-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp49.txt 4_9-4-local 0.010 3600 LocalSearch
+python main.py datasets/scp410.txt 4_10-4-local 0.010 3600 LocalSearch
